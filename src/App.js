a537e51600ce3b3fdd29a46d69dfeaa0ed27f78e
vodangkick <quang115thd@gmail.com>
2020-07-15 08:01:17 -0700

    sadkjasd

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React from 'react';
 import './App.css';
 import 'jquery';
 
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/Error";
-import { Route, Switch } from "react-router-dom";
-import NavBar from './components/NavBar';
+// import Home from "./pages/Home";
+// import Rooms from "./pages/Rooms";
+// import SingleRoom from "./pages/SingleRoom";
+// import Error from "./pages/Error";
+// import { Route, Switch } from "react-router-dom";
+// import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-      <NavBar />
+      sang
+      {/* <NavBar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/rooms" exact component={Rooms} />
         <Route path="/rooms/:slug" exact component={SingleRoom} />
         <Route component={Error} />
-      </Switch>
+      </Switch> */}
     </>
   );
 }
