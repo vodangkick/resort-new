d9a7ddef1a949e0716302496aed91d4c59d16234
vodangkick <quang115thd@gmail.com>
2020-07-17 06:16:30 -0700

    tasdnmasbd sadkjas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import 'jquery';
 
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/Error";
-import { Route, Switch } from "react-router-dom";
-import NavBar from './components/NavBar';
+// import Home from "./pages/Home";
+// import Rooms from "./pages/Rooms";
+// import SingleRoom from "./pages/SingleRoom";
+// import Error from "./pages/Error";
+// import { Route, Switch } from "react-router-dom";
+// import NavBar from './components/NavBar';
 
 function App() {
   return (
